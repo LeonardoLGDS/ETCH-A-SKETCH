7bb582a35ab052383660735c535f4a430ce778d2
Leonardo G Silva <54513066+LeonardoLGDS@users.noreply.github.com>
2021-08-23 05:00:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
+grid = document.querySelector("#container");
 
 
-grid = document.querySelector("div");
 
-for(let i = 0; i < (16 * 16); i++) {
+
+let gridSize = 16;
+
+function show_value(x)
+{
+	
+	removeAllChildNodes(document.querySelector("#container"));
+
+	document.getElementById("slider_value").innerHTML = x;
+  gridSize = x;
+
+document.querySelector("#container").style.gridTemplateColumns = "";
+document.querySelector("#container").style.gridTemplateRows = "";
+
+document.querySelector("#container").style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+document.querySelector("#container").style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+for(let i = 0; i < (gridSize * gridSize); i++) {
 	moreSqr = document.createElement("div");
-	moreSqr.classList.add("square");
+	moreSqr.classList.add("square"); 
 	grid.appendChild(moreSqr);
+  
+  let squares = document.querySelectorAll(".square");
+  
+/*   squares[i].onmouseover = () => {
+  squares[i].style.backgroundColor = "black"
+  }; */
+	
+  squares[i].onmouseover = () => {
+  squares[i].style.backgroundColor = "black"
+  };
+  
+}
 }
 
-
-document.querySelector("div.square").onmouseover = function() {mouseOver()};
-document.querySelector("div.square").onmouseout = function() {mouseOut()};
-
-function mouseOver() {
-  document.querySelector("div.square").style.backgroundColor = "black";
+function removeAllChildNodes(parent) {
+	while (parent.firstChild) {
+ 	parent.removeChild(parent.firstChild);
+	}
 }
-
-function mouseOut() {
-  document.querySelector("div.square").style.backgroundColor = "white";
+function eraseScreen() {
+	let squares = document.querySelectorAll(".square");
+  for(let i = 0; i < squares.length; i++) {
+  	squares[i].style.backgroundColor = "white";
+  }
 }
