1fec72041d303efd2d592db08a28b903173ab9d0
Leonardo G Silva <54513066+LeonardoLGDS@users.noreply.github.com>
2021-08-23 06:38:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
 grid = document.querySelector("#container");
 
-
-
-
-let gridSize = 16;
-
 function show_value(x)
 {
 	
 	removeAllChildNodes(document.querySelector("#container"));
 
 	document.getElementById("slider_value").innerHTML = x;
-  gridSize = x;
+  let gridSize = x;
 
-document.querySelector("#container").style.gridTemplateColumns = "";
-document.querySelector("#container").style.gridTemplateRows = "";
+  document.querySelector("#container").style.gridTemplateColumns = "";
+  document.querySelector("#container").style.gridTemplateRows = "";
 
-document.querySelector("#container").style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-document.querySelector("#container").style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  document.querySelector("#container").style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  document.querySelector("#container").style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
-for(let i = 0; i < (gridSize * gridSize); i++) {
-	moreSqr = document.createElement("div");
-	moreSqr.classList.add("square"); 
-	grid.appendChild(moreSqr);
-  
-  let squares = document.querySelectorAll(".square");
-  
-/*   squares[i].onmouseover = () => {
-  squares[i].style.backgroundColor = "black"
-  }; */
-	
-  squares[i].onmouseover = () => {
-  squares[i].style.backgroundColor = "black"
-  };
-  
-}
+  for(let i = 0; i < (gridSize * gridSize); i++) {
+    moreSqr = document.createElement("div");
+    moreSqr.classList.add("square"); 
+    grid.appendChild(moreSqr);
+
+    let squares = document.querySelectorAll(".square");
+
+  /*   squares[i].onmouseover = () => {
+    squares[i].style.backgroundColor = "black"
+    }; */
+
+    squares[i].onmouseover = () => {
+    squares[i].style.backgroundColor = "black"
+    };
+
+  }
 }
 
 function removeAllChildNodes(parent) {
